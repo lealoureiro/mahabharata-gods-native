4c1d8582ff1d5d4b5ea3718b229673698c3e7e9e
Leandro Loureiro <leandroloureiro@protonmail.com>
2021-06-21 10:49:27 -0500

    using NativeHint annotations to configure native image

--- src/main/java/org/leandroloureiro/mahabharatagods/Application.java
+++ src/main/java/org/leandroloureiro/mahabharatagods/Application.java
@@ -2,7 +2,9 @@ package org.leandroloureiro.mahabharatagods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
 
+@NativeHint(options = "--enable-url-protocols=http")
 @SpringBootApplication
 public class Application {
 
