a504f8b98d91b060f23a56123b4d937f76431abf
Leandro Loureiro <leandroloureiro@protonmail.com>
2021-06-24 14:50:41 -0500

    adding extra tracing attempt to define list of classes at build time

--- src/main/java/org/leandroloureiro/mahabharatagods/Application.java
+++ src/main/java/org/leandroloureiro/mahabharatagods/Application.java
@@ -2,9 +2,29 @@ package org.leandroloureiro.mahabharatagods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.InitializationHint;
+import org.springframework.nativex.hint.InitializationTime;
 import org.springframework.nativex.hint.NativeHint;
 
-@NativeHint(options = "--enable-url-protocols=http")
+@NativeHint(
+        options = {"--enable-url-protocols=http"},
+        initialization = {
+                @InitializationHint(
+                        initTime = InitializationTime.BUILD,
+                        types = {
+                                org.slf4j.impl.StaticLoggerBinder.class,
+                                org.slf4j.LoggerFactory.class,
+                                ch.qos.logback.classic.Logger.class,
+                                ch.qos.logback.core.spi.AppenderAttachableImpl.class,
+                                ch.qos.logback.core.status.StatusBase.class,
+                                ch.qos.logback.classic.Level.class,
+                                ch.qos.logback.core.status.InfoStatus.class,
+                                ch.qos.logback.classic.PatternLayout.class,
+                                ch.qos.logback.core.CoreConstants.class
+                        }
+                )
+        }
+)
 @SpringBootApplication
 public class Application {
 
